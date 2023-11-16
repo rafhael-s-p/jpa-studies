f3a14999189903862f0b13321699eb342310f4e0
Rafhael <rafhael.dev@gmail.com>
2023-11-16 10:48:18 -0300

    test: inserting an object with persist

---
+++ src/test/java/com/studies/ecommerce/TransactionalOperationsTest.java
@@ -0,0 +1,31 @@
+package com.studies.ecommerce;
+
+import com.studies.ecommerce.models.Product;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class TransactionalOperationsTest extends EntityManagerTest {
+
+    @Test
+    public void ObjectInsertion() {
+        Product product = new Product();
+
+        product.setId(2);
+        product.setName("Canon Camera");
+        product.setDescription("The best definition for your photos.");
+        product.setPrice(new BigDecimal(5000));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(product);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Product checkProduct = entityManager.find(Product.class, product.getId());
+
+        Assert.assertNotNull(checkProduct);
+    }
+
+}
