fb4ad46f8ec3a594d1de6f0a1cfbb1b9ce168d4e
Rafhael <rafhael.dev@gmail.com>
2023-11-16 10:56:05 -0300

    chore: new database register

--- src/main/resources/META-INF/database/initial-data.sql
+++ src/main/resources/META-INF/database/initial-data.sql
@@ -1,1 +1,2 @@
 insert into Product (id, name, price, description) values (1, 'Kindle', 499.0, 'Meet the new Kindle, now with adjustable built-in lighting, which allows you to read indoors or outdoors, at any time of the day.');
+insert into Product (id, name, price, description) values (3, 'GoPro Hero 7 Camera', 1400.0, '2x better performance.');
