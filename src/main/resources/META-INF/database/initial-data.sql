bf73a086ae8570766f5dc4c42568566401617c46
Rafhael <rafhael.dev@gmail.com>
2023-11-16 08:43:17 -0300

    feat: persistence files and seed data

---
+++ src/main/resources/META-INF/database/initial-data.sql
@@ -0,0 +1,1 @@
+insert into Product (id, name, price, description) values (1, 'Kindle', 499.0, 'Meet the new Kindle, now with adjustable built-in lighting, which allows you to read indoors or outdoors, at any time of the day.');
