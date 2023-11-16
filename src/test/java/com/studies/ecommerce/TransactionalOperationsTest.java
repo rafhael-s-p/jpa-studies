f2f2a540c910f7d080ac1802a3e9710e8cd6ed8d
Rafhael <rafhael.dev@gmail.com>
2023-11-16 13:51:28 -0300

    test: updating database with merge

--- src/test/java/com/studies/ecommerce/TransactionalOperationsTest.java
+++ src/test/java/com/studies/ecommerce/TransactionalOperationsTest.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 public class TransactionalOperationsTest extends EntityManagerTest {
 
     @Test
-    public void ObjectInsertion() {
+    public void objectInsert() {
         Product product = new Product();
 
         product.setId(2);
@@ -29,7 +29,28 @@ public class TransactionalOperationsTest extends EntityManagerTest {
     }
 
     @Test
-    public void ObjectRemoval() {
+    public void objectUpdate() {
+        Product product = new Product();
+
+        product.setId(1);
+        product.setName("Kindle Paperwhite");
+        product.setDescription("Meet the new Kindle.");
+        product.setPrice(new BigDecimal(599));
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(product);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Product checkProduct = entityManager.find(Product.class, product.getId());
+
+        Assert.assertNotNull(checkProduct);
+        Assert.assertEquals("Kindle Paperwhite", checkProduct.getName());
+    }
+
+    @Test
+    public void objectDelete() {
         Product product = entityManager.find(Product.class, 3);
 
         entityManager.getTransaction().begin();
