1d20f10d52bec2148d7a62ba04964d45916d4b2a
Rafhael <rafhael.dev@gmail.com>
2023-11-20 12:00:29 -0300

    test: strategies for primary key with sequence generator

--- src/test/java/com/studies/ecommerce/PrimaryKeyStrategyTest.java
+++ src/test/java/com/studies/ecommerce/PrimaryKeyStrategyTest.java
@@ -2,12 +2,30 @@ package com.studies.ecommerce;
 
 import com.studies.ecommerce.models.Category;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class PrimaryKeyStrategyTest extends EntityManagerTest {
 
+    @Ignore
     @Test
     public void autoStrategy() {
+        Category category = new Category();
+        category.setName("Electronics");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(category);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Category checkCategory = entityManager.find(Category.class, category.getId());
+
+        Assert.assertNotNull(checkCategory);
+    }
+
+    @Test
+    public void sequenceGeneratorStrategy() {
         Category category = new Category();
         category.setName("Electronics");
 
