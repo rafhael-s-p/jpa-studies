90d2504187c5f224a1ffb09cad32fc0f1a03d2f6
Rafhael <rafhael.dev@gmail.com>
2023-11-20 14:33:57 -0300

    test: strategies for primary key with identity generator

--- src/test/java/com/studies/ecommerce/PrimaryKeyStrategyTest.java
+++ src/test/java/com/studies/ecommerce/PrimaryKeyStrategyTest.java
@@ -41,8 +41,25 @@ public class PrimaryKeyStrategyTest extends EntityManagerTest {
         Assert.assertNotNull(checkCategory);
     }
 
+    @Ignore
     @Test
     public void tableGeneratorStrategy() {
+        Category category = new Category();
+        category.setName("Electronics");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(category);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Category checkCategory = entityManager.find(Category.class, category.getId());
+
+        Assert.assertNotNull(checkCategory);
+    }
+
+    @Test
+    public void identityGeneratorStrategy() {
         Category category = new Category();
         category.setName("Electronics");
 
