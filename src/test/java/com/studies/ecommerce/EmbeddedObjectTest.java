39794e1476de6235dbf6f4721ca34f429dd1ed97
Rafhael <rafhael.dev@gmail.com>
2023-11-20 11:29:01 -0300

    test: objects embedded with embeddable

---
+++ src/test/java/com/studies/ecommerce/EmbeddedObjectTest.java
@@ -0,0 +1,45 @@
+package com.studies.ecommerce;
+
+import com.studies.ecommerce.models.Address;
+import com.studies.ecommerce.models.Order;
+import com.studies.ecommerce.models.OrderStatus;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class EmbeddedObjectTest extends EntityManagerTest {
+
+    @Test
+    public void builtInObjectMapping() {
+
+        Address deliveryAddress = new Address();
+        deliveryAddress.setPostal("11111-111");
+        deliveryAddress.setStreet("First Avenue");
+        deliveryAddress.setComplement("Apt 101");
+        deliveryAddress.setNumber("100");
+        deliveryAddress.setDistrict("Downtown");
+        deliveryAddress.setCity("San Francisco");
+        deliveryAddress.setState("California");
+
+        Order order = new Order();
+        order.setId(1);
+        order.setOrderDate(LocalDateTime.now());
+        order.setStatus(OrderStatus.WAITING);
+        order.setTotal(new BigDecimal(250));
+        order.setDeliveryAddress(deliveryAddress);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(order);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Order checkOrder = entityManager.find(Order.class, order.getId());
+
+        Assert.assertNotNull(checkOrder);
+        Assert.assertNotNull(checkOrder.getDeliveryAddress());
+        Assert.assertNotNull(checkOrder.getDeliveryAddress().getPostal());
+    }
+}
