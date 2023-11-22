1035981863af0d7ebbd6385227a330ad151c23f3
Rafhael <rafhael.dev@gmail.com>
2023-11-22 10:40:54 -0300

    chore: it is possible to create an one to one relantionship intermediate table

--- src/main/java/com/studies/ecommerce/models/Invoice.java
+++ src/main/java/com/studies/ecommerce/models/Invoice.java
@@ -21,6 +21,9 @@ public class Invoice {
 
     @OneToOne
     @JoinColumn(name = "order_id")
+//    @JoinTable(name = "tab_order_invoice",
+//        joinColumns = @JoinColumn(name = "invoice_id", unique = true),
+//        inverseJoinColumns = @JoinColumn(name = "order_id", unique = true))
     private Order order;
 
     private String xml;
