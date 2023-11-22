2dd17615f6f74805b607f02020bfa2692f211b3e
Rafhael <rafhael.dev@gmail.com>
2023-11-22 09:05:45 -0300

    chore: data to test category

--- src/main/resources/META-INF/database/initial-data.sql
+++ src/main/resources/META-INF/database/initial-data.sql
@@ -7,3 +7,5 @@ insert into tab_client (id, name) values (2, 'Georgie Esther');
 insert into tab_order (id, client_id, order_date, total, status) values (1, 1, sysdate(), 200.0, 'WAITING');
 
 insert into tab_order_item (id, order_id, product_id, product_price, amount) values (1, 1, 1, 5.0, 2);
+
+insert into tab_category (id, name) values (1, 'Electronics');
