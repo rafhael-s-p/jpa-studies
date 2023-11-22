5e7dec17bc0748b7f7276ab54e82afe30757b124
Rafhael <rafhael.dev@gmail.com>
2023-11-22 08:50:02 -0300

    test: removendo objetos referenciados

---
+++ src/test/java/com/studies/ecommerce/relationship/RemovingReferencedEntitiesTest.java
@@ -0,0 +1,27 @@
+package com.studies.ecommerce.relationship;
+
+import com.studies.ecommerce.EntityManagerTest;
+import com.studies.ecommerce.models.Order;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemovingReferencedEntitiesTest extends EntityManagerTest {
+
+    @Test
+    public void removeRelatedEntity() {
+        Order order = entityManager.find(Order.class, 1);
+
+        Assert.assertFalse(order.getItems().isEmpty());
+
+        entityManager.getTransaction().begin();
+        order.getItems().forEach(i -> entityManager.remove(i));
+        entityManager.remove(order);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Order checkOrder = entityManager.find(Order.class, 1);
+
+        Assert.assertNull(checkOrder);
+    }
+}
