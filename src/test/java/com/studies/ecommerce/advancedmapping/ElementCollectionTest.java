a599d7801f3f82ce1c4a3765c985f62c6f9b5d0b
Rafhael <rafhael.dev@gmail.com>
2023-11-29 17:35:59 -0300

    test: mapping maps

--- src/test/java/com/studies/ecommerce/advancedmapping/ElementCollectionTest.java
+++ src/test/java/com/studies/ecommerce/advancedmapping/ElementCollectionTest.java
@@ -1,12 +1,14 @@
 package com.studies.ecommerce.advancedmapping;
 
 import com.studies.ecommerce.EntityManagerTest;
+import com.studies.ecommerce.models.Client;
 import com.studies.ecommerce.models.Product;
 import com.studies.ecommerce.models.ProductCharacteristic;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class ElementCollectionTest extends EntityManagerTest {
 
@@ -41,6 +43,22 @@ public class ElementCollectionTest extends EntityManagerTest {
         Product checkProduct = entityManager.find(Product.class, product.getId());
 
         Assert.assertFalse(checkProduct.getCharacteristics().isEmpty());
+    }
+
+    @Test
+    public void applyContact() {
+        entityManager.getTransaction().begin();
+
+        Client client = entityManager.find(Client.class, 1);
+        client.setContacts(Collections.singletonMap("email", "name@domain.com"));
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Client checkClient = entityManager.find(Client.class, client.getId());
+
+        Assert.assertEquals("name@domain.com", checkClient.getContacts().get("email"));
     }
 
 }
