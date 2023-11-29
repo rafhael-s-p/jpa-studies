3ad30c69137fab2ea14e3a3bef056fa242d00bc1
Rafhael <rafhael.dev@gmail.com>
2023-11-29 16:49:40 -0300

    test: built-in object collections with jpa element collection

--- src/test/java/com/studies/ecommerce/advancedmapping/ElementCollectionTest.java
+++ src/test/java/com/studies/ecommerce/advancedmapping/ElementCollectionTest.java
@@ -2,6 +2,7 @@ package com.studies.ecommerce.advancedmapping;
 
 import com.studies.ecommerce.EntityManagerTest;
 import com.studies.ecommerce.models.Product;
+import com.studies.ecommerce.models.ProductCharacteristic;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -23,6 +24,23 @@ public class ElementCollectionTest extends EntityManagerTest {
         Product checkProduct = entityManager.find(Product.class, product.getId());
 
         Assert.assertFalse(checkProduct.getTags().isEmpty());
+    }
+
+    @Test
+    public void applyCharacteristics() {
+        entityManager.getTransaction().begin();
+
+        Product product = entityManager.find(Product.class, 1);
+        product.setCharacteristics(Arrays.asList(new ProductCharacteristic("screen", "220x500"),
+                new ProductCharacteristic("color", "black")));
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Product checkProduct = entityManager.find(Product.class, product.getId());
+
+        Assert.assertFalse(checkProduct.getCharacteristics().isEmpty());
     }
 
 }
