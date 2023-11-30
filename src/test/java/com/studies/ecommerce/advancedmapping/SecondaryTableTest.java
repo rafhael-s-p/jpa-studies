f47b1dd5353b3f1ddf42734ab8462e7c4895544a
Rafhael <rafhael.dev@gmail.com>
2023-11-30 08:44:45 -0300

    test: secondary table jpa

---
+++ src/test/java/com/studies/ecommerce/advancedmapping/SecondaryTableTest.java
@@ -0,0 +1,31 @@
+package com.studies.ecommerce.advancedmapping;
+
+import com.studies.ecommerce.EntityManagerTest;
+import com.studies.ecommerce.models.Client;
+import com.studies.ecommerce.models.Gender;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class SecondaryTableTest extends EntityManagerTest {
+
+    @Test
+    public void saveClient() {
+        Client client = new Client();
+        client.setName("Lois Lane");
+        client.setGender(Gender.FEMALE);
+        client.setBirthday(LocalDate.of(1980, 8, 10));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(client);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Client checkClient = entityManager.find(Client.class, client.getId());
+
+        Assert.assertNotNull(checkClient.getGender());
+    }
+
+}
