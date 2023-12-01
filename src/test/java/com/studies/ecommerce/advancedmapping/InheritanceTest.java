4564f23ba0d34063b586b19a30df06d7f3c3a0e8
Rafhael <rafhael.dev@gmail.com>
2023-11-30 22:47:51 -0300

    test: difference between extending an abstract entity and a jpa mapped super class

--- src/test/java/com/studies/ecommerce/advancedmapping/InheritanceTest.java
+++ src/test/java/com/studies/ecommerce/advancedmapping/InheritanceTest.java
@@ -1,9 +1,11 @@
 package com.studies.ecommerce.advancedmapping;
 
 import com.studies.ecommerce.EntityManagerTest;
-import com.studies.ecommerce.models.Client;
+import com.studies.ecommerce.models.*;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 public class InheritanceTest extends EntityManagerTest {
 
@@ -21,6 +23,35 @@ public class InheritanceTest extends EntityManagerTest {
         Client checkClient = entityManager.find(Client.class, client.getId());
 
         Assert.assertNotNull(checkClient.getId());
+    }
+
+    @Test
+    public void findPayments() {
+        List<Payment> payments = entityManager
+                .createQuery("select p from Payment p")
+                .getResultList();
+
+        Assert.assertFalse(payments.isEmpty());
+    }
+
+    @Test
+    public void insertPaymentOrder() {
+        Order order = entityManager.find(Order.class, 1);
+
+        PaymentCard paymentCard = new PaymentCard();
+        paymentCard.setOrder(order);
+        paymentCard.setStatus(PaymentStatus.PROCESSING);
+        paymentCard.setCardNumber("5123716344050150");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(paymentCard);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Order checkOrder = entityManager.find(Order.class, order.getId());
+
+        Assert.assertNotNull(checkOrder.getPayment());
     }
 
 }
