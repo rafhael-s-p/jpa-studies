5b87c705889fefa0998d1e150f625beac9f21764
Rafhael <rafhael.dev@gmail.com>
2023-11-30 22:47:03 -0300

    chore: difference between extending an abstract entity and a jpa mapped super class

--- src/main/resources/META-INF/database/initial-data.sql
+++ src/main/resources/META-INF/database/initial-data.sql
@@ -4,8 +4,12 @@ insert into tab_product (id, name, price, created_at, description) values (3, 'G
 insert into tab_client (id, name) values (1, 'Jenni Jacklyn');
 insert into tab_client (id, name) values (2, 'Georgie Esther');
 
-insert into tab_order (id, client_id, created_at, total, status) values (1, 1, sysdate(), 200.0, 'WAITING');
+insert into tab_order (id, client_id, created_at, total, status) values (1, 1, sysdate(), 998.0, 'WAITING');
+insert into tab_order (id, client_id, created_at, total, status) values (2, 1, sysdate(), 499.0, 'WAITING');
 
-insert into tab_order_item (order_id, product_id, product_price, amount) values (1, 1, 5.0, 2);
+insert into tab_order_item (order_id, product_id, product_price, amount) values (1, 1, 499, 2);
+insert into tab_order_item (order_id, product_id, product_price, amount) values (2, 1, 499, 1);
+
+insert into tab_payment (order_id, status, card_number) values (1, 'PROCESSING', '5123716344050150');
 
 insert into tab_category (id, name) values (1, 'Electronics');
