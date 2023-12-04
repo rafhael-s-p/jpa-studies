b8d7c7012db47e1da1bf149a110e4275a918a0df
Rafhael <rafhael.dev@gmail.com>
2023-12-04 08:40:02 -0300

    test: table name, schema, catalog, unique constraints, indexes

---
+++ src/test/java/com/studies/ecommerce/ddl/DDLTest.java
@@ -0,0 +1,11 @@
+package com.studies.ecommerce.ddl;
+
+import com.studies.ecommerce.EntityManagerTest;
+import org.junit.Test;
+
+public class DDLTest extends EntityManagerTest {
+
+    @Test
+    public void ddlGenerate() {}
+
+}
