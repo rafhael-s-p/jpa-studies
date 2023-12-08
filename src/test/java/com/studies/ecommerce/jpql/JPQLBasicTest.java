d450668033edc86b19dba1de1eb66d2b1d127733
Rafhael <rafhael.dev@gmail.com>
2023-12-08 08:21:11 -0300

    test: typedquery vs query

--- src/test/java/com/studies/ecommerce/jpql/JPQLBasicTest.java
+++ src/test/java/com/studies/ecommerce/jpql/JPQLBasicTest.java
@@ -5,6 +5,7 @@ import com.studies.ecommerce.models.Order;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 public class JPQLBasicTest extends EntityManagerTest {
@@ -15,6 +16,19 @@ public class JPQLBasicTest extends EntityManagerTest {
                 .createQuery("select o from Order o where o.id = 1", Order.class);
         Order order = typedQuery.getSingleResult();
         Assert.assertNotNull(order);
+    }
+
+    @Test
+    public void typedQueryVsQuery() {
+        String jpql = "select o from Order o where o.id = 1";
+
+        TypedQuery<Order> typedQuery = entityManager.createQuery(jpql, Order.class);
+        Order order1 = typedQuery.getSingleResult();
+        Assert.assertNotNull(order1);
+
+        Query query = entityManager.createQuery(jpql);
+        Order order2 = (Order) query.getSingleResult();
+        Assert.assertNotNull(order2);
     }
 
 }
