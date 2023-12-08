fb9f722c56956116f7dd1ce9bdc635e797318b3d
Rafhael <rafhael.dev@gmail.com>
2023-12-08 08:41:29 -0300

    test: attribute as return

--- src/test/java/com/studies/ecommerce/jpql/JPQLBasicTest.java
+++ src/test/java/com/studies/ecommerce/jpql/JPQLBasicTest.java
@@ -1,12 +1,14 @@
 package com.studies.ecommerce.jpql;
 
 import com.studies.ecommerce.EntityManagerTest;
+import com.studies.ecommerce.models.Client;
 import com.studies.ecommerce.models.Order;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class JPQLBasicTest extends EntityManagerTest {
 
@@ -29,6 +31,20 @@ public class JPQLBasicTest extends EntityManagerTest {
         Query query = entityManager.createQuery(jpql);
         Order order2 = (Order) query.getSingleResult();
         Assert.assertNotNull(order2);
+    }
+
+    @Test
+    public void attributeReturn() {
+        String jpql = "select p.name from Product p";
+
+        TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
+        List<String> list = typedQuery.getResultList();
+        Assert.assertEquals(String.class, list.get(0).getClass());
+
+        String clientJPQL = "select o.client from Order o";
+        TypedQuery<Client> typedQueryClient = entityManager.createQuery(clientJPQL, Client.class);
+        List<Client> clientsList = typedQueryClient.getResultList();
+        Assert.assertEquals(Client.class, clientsList.get(0).getClass());
     }
 
 }
