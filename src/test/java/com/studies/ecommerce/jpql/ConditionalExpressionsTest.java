bf68238876c3f112af48ccf4b79b658738181590
Rafhael <rafhael.dev@gmail.com>
2023-12-11 10:36:42 -0300

    test: conditional expressions, is null and is empty

--- src/test/java/com/studies/ecommerce/jpql/ConditionalExpressionsTest.java
+++ src/test/java/com/studies/ecommerce/jpql/ConditionalExpressionsTest.java
@@ -10,6 +10,28 @@ import java.util.List;
 public class ConditionalExpressionsTest extends EntityManagerTest {
 
     @Test
+    public void conditionalExpressionIsNull() {
+        String jpql = "select p from Product p where p.productPhoto is null";
+        // String jpql = "select p from Product p where p.productPhoto is not null";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void conditionalExpressionIsEmpty() {
+        String jpql = "select p from Product p where p.categories is empty";
+        // String jpql = "select p from Product p where p.categories is not empty";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+    }
+
+    @Test
     public void conditionalExpressionLike() {
         String jpql = "select c from Client c where c.name like concat('%', :name, '%')";
 
