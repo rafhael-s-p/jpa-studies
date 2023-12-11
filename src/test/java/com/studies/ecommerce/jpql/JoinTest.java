8cb76601b3b9f1d267b275e8aeb6b842877acdd9
Rafhael <rafhael.dev@gmail.com>
2023-12-11 08:44:35 -0300

    test: left outer join between entities

--- src/test/java/com/studies/ecommerce/jpql/JoinTest.java
+++ src/test/java/com/studies/ecommerce/jpql/JoinTest.java
@@ -10,13 +10,23 @@ import java.util.List;
 public class JoinTest extends EntityManagerTest {
 
     @Test
+    public void leftOuterJoin() {
+        String jpql = "select o from Order o left outer join o.payment p on p.status = 'PROCESSING'";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+    }
+
+    @Test
     public void innerJoin() {
         String jpql = "select o from Order o inner join o.payment p";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
         List<Object[]> list = typedQuery.getResultList();
-        Assert.assertEquals(1, list.size());
+        Assert.assertFalse(list.isEmpty());
     }
 
 }
