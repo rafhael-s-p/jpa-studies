c6200ee058d3758f16674d1298f2f48e500ad58a
Rafhael <rafhael.dev@gmail.com>
2023-12-11 08:29:58 -0300

    chore: inner join between entities

--- src/main/resources/META-INF/database/initial-data.sql
+++ src/main/resources/META-INF/database/initial-data.sql
@@ -7,10 +7,11 @@ insert into tab_client (id, name, ssn) values (2, 'Georgie Esther', "040468979")
 insert into tab_client_detail (client_id, gender, birthday) values (1, 'FEMALE', date_sub(sysdate(), interval 31 year));
 insert into tab_client_detail (client_id, gender, birthday) values (2, 'FEMALE', date_sub(sysdate(), interval 31 year));
 
-insert into tab_order (id, client_id, created_at, total, status) values (1, 1, sysdate(), 998.0, 'WAITING');
+insert into tab_order (id, client_id, created_at, total, status) values (1, 1, date_sub(sysdate(), interval 5 day), 2398.0, 'WAITING');
 insert into tab_order (id, client_id, created_at, total, status) values (2, 1, sysdate(), 499.0, 'WAITING');
 
 insert into tab_order_item (order_id, product_id, product_price, amount) values (1, 1, 499, 2);
+insert into tab_order_item (order_id, product_id, product_price, amount) values (1, 3, 1400, 1);
 insert into tab_order_item (order_id, product_id, product_price, amount) values (2, 1, 499, 1);
 
 insert into tab_payment (order_id, status, payment_type, card_number, bar_code) values (2, 'PROCESSING', 'card', '5123716344050151', null);
