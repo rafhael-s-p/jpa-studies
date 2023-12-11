9efe53db70eabf4c7ebb866c8788dc077ae57f6c
Rafhael <rafhael.dev@gmail.com>
2023-12-11 08:58:27 -0300

    test: join fetch between entities

--- src/test/java/com/studies/ecommerce/jpql/JoinTest.java
+++ src/test/java/com/studies/ecommerce/jpql/JoinTest.java
@@ -1,6 +1,7 @@
 package com.studies.ecommerce.jpql;
 
 import com.studies.ecommerce.EntityManagerTest;
+import com.studies.ecommerce.models.Order;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,6 +9,19 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class JoinTest extends EntityManagerTest {
+
+    @Test
+    public void joinFetch() {
+        String jpql = "select o from Order o "
+                + "left join fetch o.payment "
+                + "join fetch o.client "
+                + "left join fetch o.invoice ";
+
+        TypedQuery<Order> typedQuery = entityManager.createQuery(jpql, Order.class);
+
+        List<Order> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+    }
 
     @Test
     public void leftOuterJoin() {
