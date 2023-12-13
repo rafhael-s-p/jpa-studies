a37dd8f38443b05e81548659e0d639fe246a927f
Rafhael <rafhael.dev@gmail.com>
2023-12-13 08:12:20 -0300

    test: string functions

---
+++ src/test/java/com/studies/ecommerce/jpql/StringFunctionsTest.java
@@ -0,0 +1,27 @@
+package com.studies.ecommerce.jpql;
+
+import com.studies.ecommerce.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class StringFunctionsTest extends EntityManagerTest {
+
+    @Test
+    public void functionApply() {
+        // concat, length, locate, substring, lower, upper, trim
+
+        String jpql = "select c.name, length(c.name) from Category c " +
+                " where substring(c.name, 1, 1) = 'S'";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+
+        list.forEach(arr -> System.out.println(arr[0] + " - " + arr[1]));
+    }
+
+}
