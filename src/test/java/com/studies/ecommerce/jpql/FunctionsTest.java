e788a42118fa4af88a1dee827be13fb853e5eb30
Rafhael <rafhael.dev@gmail.com>
2023-12-13 08:29:36 -0300

    test: date functions

--- src/test/java/com/studies/ecommerce/jpql/FunctionsTest.java
+++ src/test/java/com/studies/ecommerce/jpql/FunctionsTest.java
@@ -10,6 +10,23 @@ import java.util.List;
 public class FunctionsTest extends EntityManagerTest {
 
     @Test
+    public void dateFunction() {
+        // TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        // current_date, current_time, current_timestamp
+        // year(o.createdAt), month(o.createdAt), day(o.createdAt)
+
+        String jpql = "select hour(o.createdAt), minute(o.createdAt), second(o.createdAt) " +
+                " from Order o where hour(o.createdAt) > 1";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+
+        list.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
+
+    @Test
     public void stringFunction() {
         // concat, length, locate, substring, lower, upper, trim
 
