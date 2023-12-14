c6b23471969d95d7e7a3059fdc2a7e40a29a1097
Rafhael <rafhael.dev@gmail.com>
2023-12-14 16:09:57 -0300

    chore: adding data

--- src/main/resources/META-INF/database/initial-data.sql
+++ src/main/resources/META-INF/database/initial-data.sql
@@ -1,5 +1,6 @@
 insert into tab_product (id, name, price, created_at, description) values (1, 'Kindle', 499.0, date_sub(sysdate(), interval 1 day), 'Meet the new Kindle, now with adjustable built-in lighting, which allows you to read indoors or outdoors, at any time of the day.');
 insert into tab_product (id, name, price, created_at, description) values (3, 'GoPro Hero 7 Camera', 1400.0, date_sub(sysdate(), interval 1 day), '2x better performance.');
+insert into tab_product (id, name, price, created_at, description) values (4, 'Canon 80D Camera', 3500.0, sysdate(), 'The best focus adjustment.');
 
 insert into tab_client (id, name, ssn) values (1, 'Jenni Jacklyn', "051781594");
 insert into tab_client (id, name, ssn) values (2, 'Georgie Esther', "040468979");
@@ -9,10 +10,14 @@ insert into tab_client_detail (client_id, gender, birthday) values (2, 'FEMALE',
 
 insert into tab_order (id, client_id, created_at, total, status) values (1, 1, date_sub(sysdate(), interval 5 day), 2398.0, 'WAITING');
 insert into tab_order (id, client_id, created_at, total, status) values (2, 1, sysdate(), 499.0, 'WAITING');
+insert into tab_order (id, client_id, created_at, total, status) values (3, 1, date_sub(sysdate(), interval 4 day), 3500.0, 'PAID');
+insert into tab_order (id, client_id, created_at, total, status) values (4, 2, date_sub(sysdate(), interval 2 day), 499.0, 'PAID');
 
 insert into tab_order_item (order_id, product_id, product_price, amount) values (1, 1, 499, 2);
 insert into tab_order_item (order_id, product_id, product_price, amount) values (1, 3, 1400, 1);
 insert into tab_order_item (order_id, product_id, product_price, amount) values (2, 1, 499, 1);
+insert into tab_order_item (order_id, product_id, product_price, amount) values (3, 4, 3500, 1);
+insert into tab_order_item (order_id, product_id, product_price, amount) values (4, 1, 499, 1);
 
 insert into tab_payment (order_id, status, payment_type, card_number, bar_code) values (2, 'PROCESSING', 'card', '5123716344050151', null);
 
@@ -25,5 +30,8 @@ insert into tab_category (name) values ('Soccer');
 insert into tab_category (name) values ('Swimming');
 insert into tab_category (name) values ('Laptops');
 insert into tab_category (name) values ('Smartphones');
+insert into tab_category (name) values ('Cameras');
 
 insert into tab_product_category (product_id, category_id) values (1, 2);
+insert into tab_product_category (product_id, category_id) values (3, 8);
+insert into tab_product_category (product_id, category_id) values (4, 8);
