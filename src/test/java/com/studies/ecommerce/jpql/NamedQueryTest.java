fa78d563ca285a35750b2771e658eb0af7f29fb9
Rafhael <rafhael.dev@gmail.com>
2023-12-19 09:12:51 -0300

    test: named query through xml files

--- src/test/java/com/studies/ecommerce/jpql/NamedQueryTest.java
+++ src/test/java/com/studies/ecommerce/jpql/NamedQueryTest.java
@@ -1,6 +1,7 @@
 package com.studies.ecommerce.jpql;
 
 import com.studies.ecommerce.EntityManagerTest;
+import com.studies.ecommerce.models.Order;
 import com.studies.ecommerce.models.Product;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,6 +10,36 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class NamedQueryTest extends EntityManagerTest {
+
+    @Test
+    public void productXmlNamedQuery() {
+        TypedQuery<Product> typedQuery = entityManager
+                .createNamedQuery("Product.all", Product.class);
+
+        List<Product> list = typedQuery.getResultList();
+
+        Assert.assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void orderXmlNamedQuery() {
+        TypedQuery<Order> typedQuery = entityManager
+                .createNamedQuery("Order.all", Order.class);
+
+        List<Order> list = typedQuery.getResultList();
+
+        Assert.assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void xmlNamedQuery() {
+        TypedQuery<Order> typedQuery = entityManager
+                .createNamedQuery("Order.list", Order.class);
+
+        List<Order> list = typedQuery.getResultList();
+
+        Assert.assertFalse(list.isEmpty());
+    }
 
     @Test
     public void namedQuery() {
