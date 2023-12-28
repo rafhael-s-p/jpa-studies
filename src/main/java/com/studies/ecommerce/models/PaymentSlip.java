34a2687a12b91389c3796c76082c5dfb394365b3
Rafhael <rafhael.dev@gmail.com>
2023-12-28 11:48:48 -0300

    feat: criteria data functions

--- src/main/java/com/studies/ecommerce/models/PaymentSlip.java
+++ src/main/java/com/studies/ecommerce/models/PaymentSlip.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -15,5 +16,8 @@ public class PaymentSlip extends Payment {
 
     @Column(name = "bar_code", length = 100)
     private String barCode;
+
+    @Column(name = "due_date")
+    private LocalDate dueDate;
 
 }
