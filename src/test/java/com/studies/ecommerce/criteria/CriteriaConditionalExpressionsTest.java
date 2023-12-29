9987e60b5462f632c771224254135264f4539d04
Rafhael <rafhael.dev@gmail.com>
2023-12-28 21:11:00 -0300

    test: renaming

--- src/test/java/com/studies/ecommerce/criteria/CriteriaConditionalExpressionsTest.java
+++ src/test/java/com/studies/ecommerce/criteria/CriteriaConditionalExpressionsTest.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class ConditionalExpressionsCriteriaTest extends EntityManagerTest {
+public class CriteriaConditionalExpressionsTest extends EntityManagerTest {
 
     @Test
     public void conditionalExpressionCriteriaNotEqual() {
