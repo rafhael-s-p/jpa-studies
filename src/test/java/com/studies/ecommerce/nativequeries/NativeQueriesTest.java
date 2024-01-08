cc5432e7fc10defb095785df84fdc509f499e994
Rafhael <rafhael.dev@gmail.com>
2024-01-08 14:00:38 -0300

    test: passing parameters

--- src/test/java/com/studies/ecommerce/nativequeries/NativeQueriesTest.java
+++ src/test/java/com/studies/ecommerce/nativequeries/NativeQueriesTest.java
@@ -10,6 +10,22 @@ import java.util.List;
 public class NativeQueriesTest extends EntityManagerTest {
 
     @Test
+    public void passingParameters() {
+        String sql = "select prd_id id, prd_name name, prd_description description, " +
+        "            prd_created_at created_at, prd_updated_at updated_at, " +
+        "            prd_price price, prd_product_photo product_photo " +
+        " from tab_product_ecm where prd_id = :id";
+
+        Query query = entityManager.createNativeQuery(sql, Product.class);
+        query.setParameter("id", 201);
+
+        List<Product> list = query.getResultList();
+
+        list.stream().forEach(obj -> System.out.println(
+                String.format("Product => ID: %s, Nome: %s", obj.getId(), obj.getName())));
+    }
+
+    @Test
     public void executeNativeSQLAndReturnEntity() {
 //        String sql = "select id, name, price, created_at, updated_at, description, product_photo" +
 //                " from tab_product_store";
