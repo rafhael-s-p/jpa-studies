a6193ffcdfdd30f085486a50c208b03ce9747c05
Rafhael <rafhael.dev@gmail.com>
2024-01-09 10:31:14 -0300

    feat: named native query using xml file returning dto

---
+++ src/main/java/com/studies/ecommerce/dto/CategoryDTO.java
@@ -0,0 +1,17 @@
+package com.studies.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+
+    private Integer id;
+
+    private String name;
+}
