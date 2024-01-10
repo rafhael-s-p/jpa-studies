e9282e15ab567ceade77f7f5c3bb2353fbff3f48
Rafhael <rafhael.dev@gmail.com>
2024-01-10 10:41:19 -0300

    test: receiving a list of the procedure

--- src/test/java/com/studies/ecommerce/nativequeries/StoredProceduresTest.java
+++ src/test/java/com/studies/ecommerce/nativequeries/StoredProceduresTest.java
@@ -1,15 +1,31 @@
 package com.studies.ecommerce.nativequeries;
 
 import com.studies.ecommerce.EntityManagerTest;
+import com.studies.ecommerce.models.Client;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.ParameterMode;
 import javax.persistence.StoredProcedureQuery;
+import java.util.List;
 
 public class StoredProceduresTest extends EntityManagerTest {
 
     public static final Integer PRODUCT_ID = 1;
+
+    @Test
+    public void receivingListAsProcedure() {
+        StoredProcedureQuery storedProcedureQuery = entityManager
+                .createStoredProcedureQuery("clients_who_bought_above_average", Client.class);
+
+        storedProcedureQuery.registerStoredProcedureParameter( "sell_year", Integer.class, ParameterMode.IN);
+
+        storedProcedureQuery.setParameter("sell_year", 2024);
+
+        List<Client> list = storedProcedureQuery.getResultList();
+
+        Assert.assertFalse(list.isEmpty());
+    }
 
     @Test
     public void usingParametersInAndOut() {
