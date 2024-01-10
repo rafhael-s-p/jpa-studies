37a914c2118d564f7c41ecdd1d4c74b5146d9c1e
Rafhael <rafhael.dev@gmail.com>
2024-01-10 11:06:00 -0300

    chore: invoking a database view

--- src/main/resources/META-INF/database/drop.sql
+++ src/main/resources/META-INF/database/drop.sql
@@ -9,6 +9,8 @@ drop procedure if exists find_product_name;
 drop procedure if exists clients_who_bought_above_average;
 drop procedure if exists adjust_price_product;
 
+drop view if exists clients_who_bought_above_average_view;
+
 --alter table tab_category drop foreign key fk_category_categoryfather;
 --alter table tab_client_contact drop foreign key fk_client_contact_client;
 --alter table tab_client_detail drop foreign key fk_client_detail_client;
