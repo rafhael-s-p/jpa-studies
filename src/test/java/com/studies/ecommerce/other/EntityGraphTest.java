5f192a4f0b2d3cd83892f00c25c970e414f898d4
Rafhael <rafhael.dev@gmail.com>
2024-01-16 14:33:56 -0300

    test: sub graph

--- src/test/java/com/studies/ecommerce/other/EntityGraphTest.java
+++ src/test/java/com/studies/ecommerce/other/EntityGraphTest.java
@@ -1,15 +1,32 @@
 package com.studies.ecommerce.other;
 
 import com.studies.ecommerce.EntityManagerTest;
+import com.studies.ecommerce.models.Client;
 import com.studies.ecommerce.models.Order;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.EntityGraph;
+import javax.persistence.Subgraph;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class EntityGraphTest extends EntityManagerTest {
+
+    @Test
+    public void findEssentialAttributesOfOrder02() {
+        EntityGraph<Order> entityGraph = entityManager.createEntityGraph(Order.class);
+        entityGraph.addAttributeNodes(
+                "createdAt", "status", "total");
+
+        Subgraph<Client> subgraphClient = entityGraph.addSubgraph("client", Client.class);
+        subgraphClient.addAttributeNodes("name", "ssn");
+
+        TypedQuery<Order> typedQuery = entityManager.createQuery("select o from Order o", Order.class);
+        typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+        List<Order> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+    }
 
     @Test
     public void findEssentialAttributesOfOrder() {
@@ -23,8 +40,7 @@ public class EntityGraphTest extends EntityManagerTest {
         Assert.assertNotNull(order);
         */
 
-        TypedQuery<Order> typedQuery = entityManager
-                .createQuery("select o from Order o", Order.class);
+        TypedQuery<Order> typedQuery = entityManager.createQuery("select o from Order o", Order.class);
         typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
         List<Order> list = typedQuery.getResultList();
         Assert.assertFalse(list.isEmpty());
