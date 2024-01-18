db07d0fbbbc7f4e578710bb2afb5c1f3396e4b1c
Rafhael <rafhael.dev@gmail.com>
2024-01-18 09:34:32 -0300

    test: first level cache

---
+++ src/test/java/com/studies/ecommerce/cache/CacheTest.java
@@ -0,0 +1,43 @@
+package com.studies.ecommerce.cache;
+
+import com.studies.ecommerce.models.Order;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class CacheTest {
+
+    protected static EntityManagerFactory entityManagerFactory;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        entityManagerFactory = Persistence
+                .createEntityManagerFactory("Ecommerce-PU");
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() {
+        entityManagerFactory.close();
+    }
+
+
+    @Test
+    public void findFromCache() {
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+
+        System.out.println("Find from instance 1:");
+        entityManager1.find(Order.class, 1);
+
+        System.out.println("Find from instance 1 (First Level Cache):");
+        entityManager1.find(Order.class, 1);
+
+        System.out.println("Find from instance 2:");
+        entityManager2.find(Order.class, 1);
+    }
+
+}
