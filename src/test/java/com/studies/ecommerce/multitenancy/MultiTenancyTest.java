6063d4f95584448979082f8f3bf82e79cea412c9
Rafhael <rafhael.dev@gmail.com>
2024-01-25 10:07:16 -0300

    test: multi tenancy, schema approach

---
+++ src/test/java/com/studies/ecommerce/multitenancy/MultiTenancyTest.java
@@ -0,0 +1,29 @@
+package com.studies.ecommerce.multitenancy;
+
+import com.studies.ecommerce.EntityManagerFactoryTest;
+import com.studies.ecommerce.hibernate.EcmCurrentTenantIdentifierResolver;
+import com.studies.ecommerce.models.Product;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+
+public class MultiTenancyTest extends EntityManagerFactoryTest {
+
+    @Test
+    public void schemaApproach() {
+
+        EcmCurrentTenantIdentifierResolver.setTenantIdentifier("ecommerce");
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        Product product1 = entityManager1.find(Product.class, 1);
+        Assert.assertEquals("Kindle", product1.getName());
+        entityManager1.close();
+
+        EcmCurrentTenantIdentifierResolver.setTenantIdentifier("ecommerce_second_client");
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+        Product product2 = entityManager2.find(Product.class, 1);
+        Assert.assertEquals("Kindle", product2.getName());
+        entityManager2.close();
+    }
+
+}
